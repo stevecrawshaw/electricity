6192b93048cb0a9c1205905a45ee8c234c444c2d
Steve Crawshaw <murrayjarvis@gmail.com>
2025-08-12 15:53:27 +0100

    commit 1

---
+++ repd.sql
@@ -0,0 +1,42 @@
+duckdb
+LOAD SPATIAL;
+
+CREATE OR REPLACE TABLE repd_tbl AS 
+FROM read_csv('data/repd-q1-apr-2025.csv',
+normalize_names=true,
+ignore_errors=true);
+
+DESCRIBE repd_tbl;
+
+CREATE OR REPLACE TABLE weca_rep_tbl AS 
+SELECT 
+site_name
+,STRPTIME(operational, '%d/%m/%Y')::DATE AS operational_date
+,EXTRACT(YEAR FROM STRPTIME(operational, '%d/%m/%Y')::DATE) AS year
+,record_last_updated_ddmmyyyy
+,technology_type
+,storage_type
+,installed_capacity_mwelec::INT installed_capacity_mwelec
+,share_community_scheme
+,development_status
+,planning_authority
+,county
+,post_code
+,xcoordinate bng_x
+,ycoordinate bng_y
+, ST_Transform(ST_Point(xcoordinate, ycoordinate), 'EPSG:27700', 'EPSG:4326') geo_point_2d
+FROM repd_tbl
+WHERE 
+    county IN('South Gloucestershire', 'North Somerset', 'Bristol, City of', 'Bath and North East Somerset', 'Avon')
+    AND
+    installed_capacity_mwelec IS NOT NULL
+    AND 
+    development_status_short = 'Operational';
+
+
+SELECT 
+"year",
+SUM(installed_capacity_mwelec) capacity_added
+FROM weca_rep_tbl
+GROUP BY "year"
+ORDER BY "year";
